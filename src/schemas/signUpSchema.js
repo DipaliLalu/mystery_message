1e158ae67e08b4f1148a86a7e2aeb1b1326ae0c5
Dipali Lalu <dvlalu7@gamil.com>
2025-01-30 17:24:04 +0530

    unique username route maked

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -1,7 +1,8 @@
 import { z } from "zod"
+export const usernameValidation=z.string().min(2, { message: 'Username at least 2 chaeacters' }).max(20, { message: 'Username must be more than 20 characters' }).regex(/^[a-zA-Z0-9_]+$/, { message: 'Username must not contain special characters' });
 
 export const signUpSchema = z.object({
-    username: z.string().min(2, { message: 'Username at least 2 chaeacters' }).min(20, { message: 'Username must be more than 20 characters' }).regex(/^[a-zA-Z0-9_]+$/, { message: 'Username must not contain special characters' }),
+    username: usernameValidation,
     email: z.string().email({ message: "Invalid email address" }),
     password: z.string().min(6, { message: 'Password must be 6 characters' })
 })
